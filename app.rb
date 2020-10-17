9d4ecf7cf24df93ae943f60f8fe22a1738a57dd9
yani82 <yani.iman1@gmail.com>
2020-10-17 00:38:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end 
+
+    get '/name' do 
+        "My name is Yani"
+    end 
+
+    get '/hometown' do 
+        "My hometown is Kuala Lumpur"
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is Ready of Not"
+    end 
+
 end
